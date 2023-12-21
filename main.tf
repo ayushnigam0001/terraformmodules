59229a2d1eaa5dba3628619e69b83b0b269ca501
Ayush Nigam <ayushnigam0001@gmail.com>
2023-12-21 15:04:19 +0530

    commit02-formating the exesting code

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "ec2_server" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  key_name = var.key
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+  key_name                = var.key
   disable_api_termination = var.enable_termination_protection
   network_interface {
     network_interface_id = aws_network_interface.eni.id
-    device_index = 0
+    device_index         = 0
   }
   tags = {
     "Name" = var.server_name
@@ -13,7 +13,7 @@ resource "aws_instance" "ec2_server" {
 }
 
 resource "aws_network_interface" "eni" {
-  subnet_id = var.subnet_id
+  subnet_id       = var.subnet_id
   security_groups = var.security_group_list
   tags = {
     "Name" = "eni-${var.server_name}"
--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "ami_id" {
   type = string
   validation {
-    condition = can(regex("^ami-",var.ami_id))
+    condition     = can(regex("^ami-", var.ami_id))
     error_message = "Invalid Image Id"
   }
 }
 variable "instance_type" {
   type = string
   validation {
-    condition = can(regex("t2.micro|t2.medium", var.instance_type))
+    condition     = can(regex("t2.micro|t2.medium", var.instance_type))
     error_message = "Only t2.micro and t2.medium are permited"
   }
 }
 variable "subnet_id" {
   type = string
   validation {
-    condition = can(regex("^subnet-", var.subnet_id))
+    condition     = can(regex("^subnet-", var.subnet_id))
     error_message = "Invalid Subnet Id"
   }
 }
@@ -23,7 +23,7 @@ variable "key" {
   type = string
 }
 variable "enable_termination_protection" {
-  type = bool
+  type    = bool
   default = false
 }
 variable "security_group_list" {
@@ -31,5 +31,5 @@ variable "security_group_list" {
 }
 
 variable "server_name" {
-    type = string
+  type = string
 }
