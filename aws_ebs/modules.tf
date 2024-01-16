f7d5fe1c1c11a3f508214461d5b5c152e50f3703
Ayush Nigam <ayushnigam0001@gmail.com>
2024-01-16 22:45:02 +0530

    fixed err

--- aws_ebs/modules.tf
+++ aws_ebs/modules.tf
@@ -2,7 +2,5 @@ resource "aws_ebs_volume" "example" {
   availability_zone = var.availability_zone
   size              = 40
   type = "gp3"
-  tags = {
-    Name = merge(var.regular_tag, var.additional_tags)
-  }
+  tags = merge(var.regular_tag, var.additional_tags)
 }
